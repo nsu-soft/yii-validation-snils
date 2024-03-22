86255bfc1f41fbf5d3f0e34e9371f315ba58ad7d
Michael Naumov <vommuan@gmail.com>
2024-03-22 12:49:22 +0700

    Unit test. SnilsValidator::testSpecifyingErrorMessage()

--- tests/Unit/SnilsValidatorTest.php
+++ tests/Unit/SnilsValidatorTest.php
@@ -11,20 +11,27 @@ class SnilsValidatorTest extends Unit
 {
     protected UnitTester $tester;
 
-    protected ?SnilsValidator $validator;
-
     protected function _before()
     {
-        $this->validator = new SnilsValidator();
     }
 
-    public function testValidationSuccess()
+    public function testValidationSuccess(): void
     {
-        $this->assertTrue($this->validator->validate('72340627370'));
+        $validator = new SnilsValidator();
+        $this->assertTrue($validator->validate('72340627370'));
     }
 
-    public function testValidationError()
+    public function testValidationError(): void
     {
-        $this->assertFalse($this->validator->validate('72340627371'));
+        $validator = new SnilsValidator();
+        $this->assertFalse($validator->validate('72340627371'));
+    }
+
+    public function testSpecifyingErrorMessage(): void
+    {
+        $message = 'Incorrect SNILS';
+        $validator = new SnilsValidator(['message' => $message]);
+        $validator->validate('72340627371', $error);
+        $this->assertEquals($message, $error);
     }
 }
